33acfb1602e219208241168c2d45faa4ccf2ba3e
JinYoung091 <louis9157@naver.com>
2024-06-05 11:14:50 +0900

    update

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const modal = document.querySelector('.modal');
 const btnOpenModal = document.querySelector('.btn-open-modal');
-const btncloseModal = document.querySelector('.btn-close-modal');
+const btnCloseModal = document.querySelector('.btn-close-modal');
 
-//로그인 버튼을 눌렀을때 모달 띄우기
+// 로그인 버튼을 눌렀을 때 모달 띄우기
 btnOpenModal.addEventListener("click", () => {
     modal.style.display = "flex";
     document.body.classList.add("stop-scroll");
 });
-//모달 버튼 눌러 닫기
-btncloseModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-    document.body.classList.remove("stop-scroll");
+
+// 모달 닫기 버튼을 눌렀을 때 모달 닫기
+btnCloseModal.addEventListener('click', () => {
+    closeModal();
 });
-//모달 밖을 눌러 닫기
+
+// 모달 밖을 눌렀을 때 모달 닫기
 window.addEventListener('click', (e) => {
     if (e.target === modal) {
-        modal.style.display = 'none';
-        ocument.body.classList.remove("stop-scroll");
+        closeModal();
     }
 });
 
+// ESC 키를 눌렀을 때 모달 닫기
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+});
+
+// 모달 닫기 함수
+function closeModal() {
+    modal.style.display = 'none';
+    document.body.classList.remove("stop-scroll");
+}
